e1f0559052b8f0e66fa2b77b10d19c9620ade103
Sultana Razia <sultanarazia483@gmail.com>
2024-06-12 12:42:58 +0600

    Add Bookmark Button And Lift Up The State

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,12 @@
+import { CiBookmark } from "react-icons/ci";
 import PropTypes from 'prop-types';
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, handleAddToBookmark }) => {
     const { cover, title, reading_time, author, author_img, posted_date, hashtags } = blog;
     return (
-        <div>
-            <img src={cover} alt={`Cover picture of thr title ${title}`} />
-            <div className='flex justify-between'>
+        <div className='mb-20'>
+            <img className='w-full h-[450px] mb-8' src={cover} alt={`Cover picture of thr title ${title}`} />
+            <div className='flex justify-between mb-4'>
                 <div className='flex '>
                     <img className='w-14' src={author_img} alt="" />
                     <div className='ml-6'>
@@ -15,6 +16,7 @@ const Blog = ({ blog }) => {
                 </div>
                 <div>
                     <span>{reading_time} min read</span>
+                    <button onClick={handleAddToBookmark} className="ml-2"><CiBookmark /></button>
                 </div>
             </div>
             <h2 className='text-4xl'>{title}</h2>
